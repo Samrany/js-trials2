724c507e91cb8eb1e0b510ac0a3278c3f75b0078
Shira Amrany <shira.amrany@gmail.com>
2020-05-19 23:37:34 +0000

    arrays done

--- arrays.js
+++ arrays.js
@@ -3,17 +3,37 @@
 
 // 1. printIndices
 function printIndices(items) {
+	for (const idx in items) {
+		console.log(items[idx], idx)
+
+	}
   // Replace this with your code
 }
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+
+  const result = [];
+
+  for (const idx in items) {
+  	if (idx % 2 === 0) {
+  		result.push(items[idx])
+  	}
+  }
+  return result;
 }
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+	const output = [];
+	items.sort((a, b) => a - b);
+
+	for (const idx in items) {
+		if (idx < n) {
+			output.push(items[idx])
+		}
+	}
+	return output
 }
